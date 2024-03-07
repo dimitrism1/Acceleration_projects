b91697a6543836855f7d7a81127b7bcb4e08227e
dimitrism <mitsasd@gmail.com>
2024-03-07 23:02:23 +0200

    Time measurement host

--- src/hostcl.cpp
+++ src/hostcl.cpp
@@ -4,10 +4,10 @@
 #include "xcl2.hpp"
 
 
-#define ROWA 30
+#define ROWA 7
 #define COLA 2
 #define COLB 2
-#define iter 1
+#define iter 1000
 
 int main(int argc,char** argv){
 
@@ -16,7 +16,6 @@ int main(int argc,char** argv){
 static const int DATA_SIZE=2048;
 size_t size_in_bytes=DATA_SIZE*sizeof(int);
 
-std::cout<<"hey1"<<std::endl;
 cl_int err;
 unsigned nb;
 /*std::vector<cl::Platform> platforms1;
@@ -37,7 +36,7 @@ std::cout<<devices[i].getInfo<CL_DEVICE_NAME>()<<std::endl;
 ////////Read binary file and point to its buffer
 auto filebuf = xcl::read_binary_file(binaryfile,nb);
 cl::Context context;
-//cl::Program::Binaries bins;
+//cl::Program::Binaries bins;				//used if bins.push_back is needed
 cl::Program program;
 cl::CommandQueue q;
 cl::Kernel kernel_matmul;
@@ -71,13 +70,14 @@ break;
 }
 
 }
-buffera = cl::Buffer(context,CL_MEM_READ_ONLY,size_in_bytes,nullptr,&err);		//constructor->cant use buffera.Buffer to access member
+
+buffera = cl::Buffer(context,CL_MEM_READ_ONLY,size_in_bytes,nullptr,&err);		
 bufferb = cl::Buffer(context,CL_MEM_READ_ONLY,size_in_bytes,nullptr,&err);
 bufferout = cl::Buffer(context,CL_MEM_WRITE_ONLY,size_in_bytes,nullptr,&err);
 
 ////// Set kernel arguments /////////
 int narg=0;
-kernel_matmul.setArg(narg++,buffera);						//function not constructor=can use .
+kernel_matmul.setArg(narg++,buffera);						
 kernel_matmul.setArg(narg++,bufferb);
 kernel_matmul.setArg(narg++,bufferout);
 kernel_matmul.setArg(narg++,rowa);
@@ -92,14 +92,15 @@ ptr_a=(int*)q.enqueueMapBuffer(buffera,CL_TRUE,CL_MAP_WRITE,0,size_in_bytes,null
 ptr_b=(int*)q.enqueueMapBuffer(bufferb,CL_TRUE,CL_MAP_WRITE,0,size_in_bytes,nullptr,nullptr,&err);
 ptr_out=(int*)q.enqueueMapBuffer(bufferout,CL_TRUE,CL_MAP_READ,0,size_in_bytes,nullptr,nullptr,&err);
 
-clock_t t_fpga1;
-t_fpga1=clock();
-const auto fpga_start{std::chrono::steady_clock::now()};
+
+////// Measure time for the whole fpga execution ////////
+clock_t fpga_start,fpga_clk;
+fpga_start=clock();
 ///////// Initialise pointers //////////
 for(int o=0;o<iter;o++){
 for(int i=0;i<DATA_SIZE;i++){
-ptr_a[i]=1;//rand()%DATA_SIZE;
-ptr_b[i]=1;//rand()%DATA_SIZE;
+ptr_a[i]=1;
+ptr_b[i]=1;
 
 
 }
@@ -107,16 +108,10 @@ ptr_b[i]=1;//rand()%DATA_SIZE;
 q.enqueueMigrateMemObjects({buffera,bufferb},0);//CL_MIGRATE_MEM_OBJECT_CONTENT_UNDEFINED);
 
 cl::Event event;
-uint64_t timestart,timeend;
 //////// Start kernel /////////
 q.enqueueNDRangeKernel(kernel_matmul,1,1,1,nullptr,&event);			//enqueueTask is deprecated, enqueueNDRangeKernel equivalent
 //q.enqueueTask(kernel_matmul);
 q.finish();
-/*event.getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_START, &timestart);
-event.getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_END, &timeend);
-auto simple_time = (timeend - timestart)*10e-09;
-
-std::cout<<"Simple time:"<< simple_time<<" seconds"<<std::endl;*/
 
 /////// send data back to host //////////
 q.enqueueMigrateMemObjects({bufferout},CL_MIGRATE_MEM_OBJECT_HOST);
@@ -125,8 +120,8 @@ q.enqueueMigrateMemObjects({bufferout},CL_MIGRATE_MEM_OBJECT_HOST);
 
 q.finish();		//wait until all previous commands have been executed
 
-/*q.enqueueUnmapMemObject(buffera,ptr_a,nullptr,nullptr);
-q.enqueueUnmapMemObject(bufferb,ptr_b,nullptr,nullptr);*/
+q.enqueueUnmapMemObject(buffera,ptr_a,nullptr,nullptr);
+q.enqueueUnmapMemObject(bufferb,ptr_b,nullptr,nullptr);
 
 
 ////// print hardware result //////////
@@ -150,15 +145,14 @@ break;
 }
 }
 
-t_fpga1=clock()-t_fpga1;
-const auto fpga_end {std::chrono::steady_clock::now()};
-const std::chrono::duration<double> fpga_time{fpga_end-fpga_start};
 
+fpga_clk=clock()-fpga_start;
+float fpga_time = (float)fpga_clk/(CLOCKS_PER_SEC);
 
 //////////// Calculate software result ////////////
-clock_t t;
-t=clock();
-const auto cpu_start{std::chrono::steady_clock::now()};
+clock_t cpu_start,cpu_time;
+cpu_start=clock();
+
 int host_result[rowa*colb]={0};
 for(int p = 0; p<iter; p++){
 std::cout<<"Printing software results"<<std::endl;
@@ -189,21 +183,20 @@ for(int i = 0;i < rowa;i++){
 }
 }
 std::cout<<std::endl;	
-const auto cpu_end {std::chrono::steady_clock::now()};
-t=clock()-t;
+cpu_time=clock()-cpu_start;
 ////////// Performance output /////////////
-std::cout<<"CPU time first:"<<t/float(CLOCKS_PER_SEC)<<" seconds"<<std::endl;
-
-const std::chrono::duration<double> start_time{cpu_end-cpu_start};
+std::cout<<"CPU duration:"<<cpu_time/float(CLOCKS_PER_SEC)<<" seconds"<<std::endl;
 int total=(2*rowa*cola*colb-rowa*colb)*iter*sizeof(int);
 std::cout<<"TB= "<<total<<std::endl;
-std::cout<<"FPGA throughput: "<<(total/(fpga_time.count()*100))/(1024*1024)<<"MB/s "<<std::endl;
-std::cout<<"CPU duration is "<<start_time.count()*100<<" Seconds"<<std::endl;
-std::cout<<"FPGA duration is "<<fpga_time.count()*100<<" Seconds"<<std::endl;
-std::cout<<"Speedup is "<<(double)start_time.count()/(double)fpga_time.count()<<std::endl;
+std::cout << "FPGA duration:" << fpga_time << std::endl;
+std::cout << "Throughput:" << (total/fpga_time)/(1024*1024) << " MB/s" << std::endl;
+//std::cout<<"Speedup is "<<(double)start_time.count()/(double)fpga_time.count()<<std::endl;
 
-clock_t t_fpga,t_fpga_start;
-t_fpga_start=clock();
+
+
+
+////////////// Alternative test way to measure fpga speed ///////////////////
+
 cl::Event event;
   uint64_t nstimestart, nstimeend;
   double fpga_exec_time_s=0;
@@ -218,15 +211,11 @@ cl::Event event;
   OCL_CHECK(err, err = event.getProfilingInfo<uint64_t>(
                      CL_PROFILING_COMMAND_END, &nstimeend));
   duration_nanosec = nstimeend - nstimestart;
-  fpga_exec_time_s += double((duration_nanosec * (1.0e-9))); // conversion to miliseconds
+  fpga_exec_time_s += double((duration_nanosec * (1.0e-9))); // conversion to seconds
   }
   avg_fpga_exec_time = fpga_exec_time_s/iter;
-std::cout<<"Fpga exec time: "<<fpga_exec_time_s<<" seconds"<<std::endl;
-std::cout<<"throughput:"<<(total/fpga_exec_time_s)/(1024*1024)<<" MB/s "<<std::endl;
-t_fpga=float(clock()-t_fpga_start);
-std::cout<<"FPGA time first:"<<t_fpga/float(CLOCKS_PER_SEC)<<" seconds"<<std::endl;
-std::cout<<"FPGA time second:"<<t_fpga1/float(CLOCKS_PER_SEC)<<" seconds"<<std::endl;
-std::cout<<"throughput first:"<<(total/t_fpga)/(1024*1024)<<" MB/s "<<std::endl;
+std::cout<<"alt_Fpga exec time: "<<fpga_exec_time_s<<" seconds"<<std::endl;
+std::cout<<"alt_throughput:"<<(total/fpga_exec_time_s)/(1024*1024)<<" MB/s "<<std::endl;
 
 /*q.enqueueUnmapMemObject(buffera,ptr_a,nullptr,nullptr);
 q.enqueueUnmapMemObject(bufferb,ptr_b,nullptr,nullptr);
