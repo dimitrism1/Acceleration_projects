c5136bc8f2fa68fe5fbbeee2d1a22a7e3d9b4e54
dimitrism <mitsasd@gmail.com>
2024-03-21 13:22:06 +0200

    Added new kernel

---
+++ krnl_src/matmul_cor.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#define ROWA 16			//used only for the testbench
+#define COLA 16
+#define COLB 16
+
+#define MAX_SIZE 16
+extern "C"{
+
+/*void matmul(uint32_t a[][COLA],uint32_t b[][COLB],uint32_t out[][COLB]);//,uint32_t rowa,uint32_t cola,uint32_t colb);
+}*/
+void matmul_cor(uint32_t* a,uint32_t* b,uint32_t* out,uint32_t a_row,uint32_t a_col,uint32_t b_col,uint32_t iter);
+}
